8a28bd39ac7c73aef0f01f82243ad066ae89ba38
Marius Phillips <mariusphillips@Mariuss-MacBook-Pro.local>
2023-07-02 16:56:21 +0200

    render some dummy-projects using bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap'
+import { sampleProduct } from './data'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
-      <header>Frisbee golf</header>
-      <main></main>
-      <footer>All rights reserved</footer>
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">Frisbee golf</Navbar.Brand>
+          </Container>
+          <Nav>
+            <Nav.Link href="#link">Products</Nav.Link>
+            <Nav.Link href="#link">Cart</Nav.Link>
+          </Nav>
+        </Navbar>
+      </header>
+      <main>
+        <Container className="mt-4">
+          <Row>
+            {sampleProduct.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3}>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="product-image"
+                />
+                <h3>{product.name}</h3>
+                <p>${product.price}</p>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </main>
+      <footer>
+        <div className="text-center">All rights reserved</div>
+      </footer>
     </div>
   )
 }
