822c551c9375033a3110faa0080559b2694be71a
Marius Phillips <mariusphillips@Mariuss-MacBook-Pro.local>
2023-07-03 23:31:47 +0200

    fetch product by connecting endpoints to frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,6 +12,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import HomePage from './pages/HomePage.tsx'
 import ProductPage from './pages/ProductPage.tsx'
+import axios from 'axios'
+
+axios.defaults.baseURL =
+  process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : '/'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,11 +1,67 @@
 import { Col, Row } from 'react-bootstrap'
-import { sampleProduct } from '../data'
 import { Link } from 'react-router-dom'
+import React, { useEffect, useReducer } from 'react'
+import { Product } from '../types/Product'
+import axios from 'axios'
+import { getError } from '../utils'
+import { ApiError } from '../types/ApiError'
+import LoadingBox from '../components/LoadingBox'
+import MessageBox from '../components/MessageBox'
+
+type State = {
+  products: Product[]
+  loading: boolean
+  error: string
+}
+
+type Action =
+  | { type: 'FETCH_REQUEST' }
+  | { type: 'FETCH_SUCCESS'; payload: Product[] }
+  | { type: 'FETCH_FAIL'; payload: string }
+
+const initialState: State = {
+  products: [],
+  loading: true,
+  error: '',
+}
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true }
+    case 'FETCH_SUCCESS':
+      return { ...state, loading: false, products: action.payload }
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
 
 export default function HomePage() {
-  return (
+  const [{ loading, products, error }, dispatch] = useReducer<
+    React.Reducer<State, Action>
+  >(reducer, initialState)
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const result = await axios.get('/api/products')
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err as ApiError) })
+      }
+    }
+    fetchProducts()
+  }, [])
+
+  return loading ? (
+    <LoadingBox />
+  ) : error ? (
+    <MessageBox variant="danger">{error}</MessageBox>
+  ) : (
     <Row>
-      {sampleProduct.map((product) => (
+      {products.map((product) => (
         <Col key={product.slug} sm={6} md={4} lg={3}>
           <Link to={`/product/${product.slug}`}>
             <img
